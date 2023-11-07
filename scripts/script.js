d2435377381ef441b94594fabadaa80dc0e8d3e0
justindebruin <149573719+justindebruin@users.noreply.github.com>
2023-11-07 14:59:54 +0100

    Semi-final

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 // JavaScript Document
 
+
+/********************/
+/* MOVIE LOGO ARRAY */
+/********************/
+
+
 // Logo swap met Upgrade - Gemaakt met hulp van Bahaa//
 
-// array voor het cyclen van movie-logo's//
 var moviesLogos = [                       
   "/images/Movie Logos/iron-man logo.png",
   "/images/Movie Logos/iron-man logo.png",
@@ -29,14 +34,14 @@ var count = 0;
 
 // console.log(count);
 
-var upgradeButton = document.querySelector("article ol button");    // Upgrade button //
+var upgradeButton = document.querySelector("article ul button");    // Upgrade button //
 var movieLogo = document.querySelector("h1 img");                   // Movie logo //
 
 upgradeButton.onclick = combinedNextSuit;
 
 function imageNaarVolgendLogo() {
 
-  if (count >= 18) {
+  if (count >= 19) {
     count = 0;
     console.log(count);
     console.log(moviesLogos[count]);
@@ -52,7 +57,14 @@ function imageNaarVolgendLogo() {
 
 
 
-// array voor het cyclen van Iron Man suits//
+
+
+
+/************************/
+/* IRON MAN SUITS ARRAY */
+/************************/
+
+
 var ironManSuitCycle = [                       
   // "/images/Iron Man suits/mark 1.png",
   "/images/Iron Man suits/mark 2.png",
@@ -77,15 +89,15 @@ var ironManSuitCycle = [
 
 // console.log(count);
 
-var upgradeButtonSuits = document.querySelector("article ol button")                    // Upgrade button //
-var ironManSuit = document.querySelector("article ol li:nth-of-type(1) img")       // Iron Man Suit //
-var suitFlying = document.querySelector("article ol li:nth-of-type(1) img")        // Suit fly-in animation //
+var upgradeButtonSuits = document.querySelector("article ul button")                    // Upgrade button //
+var ironManSuit = document.querySelector("article ul li:nth-of-type(1) img")       // Iron Man Suit //
+var suitFlying = document.querySelector("article ul li:nth-of-type(1) img")        // Suit fly-in animation //
 
 upgradeButtonSuits.onclick = combinedNextSuit
 
 function imageNaarVolgendSuit() {
 
-  if (count>= 18) {
+  if (count>= 19) {
     window.location.reload();
     console.log(count);
     console.log(ironManSuitCycle[count]);
@@ -111,10 +123,20 @@ function suitFlyAnimation() {
   
 }
 
-// geluid-afspelen //
 
-var soundButton = document.querySelector("article ol button")
-var sound = document.querySelector("article ol audio")
+
+
+
+
+
+
+
+/*******************/
+/* GELUID AFSPELEN */
+/*******************/
+
+var soundButton = document.querySelector("article ul:nth-of-type(2) button")
+var sound = document.querySelector("article ul audio")
 
 soundButton.onclick = combinedNextSuit;
 
@@ -130,10 +152,213 @@ function mechanicalSounds() {
   }
 }
 
+
+
+
+
+
+/****************/
+/* TEXT BUBBLES */
+/****************/
+
+
+// text swap HEAD //
+var textHead = [
+  "Very bulky suit of strong armour, fitted with some rockets and flamethrowers.",
+  "Added a HUD (heads-up display)",
+  "Added wrist-mounted missiles, shoulder-mounted machine guns and deployable flares.",
+  "Uses a brighter shade of red.",
+  "Prioritized portability over strength and endurance.",
+  "Introduced a grenade launcher, missile launcher and lasers into the suit.",
+  "Brought back the original circular hole for the arc reactor.",
+  "First suit specifically designed for stealth purposes.",
+  "Has electronic cloaking and colour camouflaging built into the suit for stealth operations.",
+  "Features an enhanced energy system.",
+  "First suit able to reach speeds over Mach 5 (6174 km/h).",
+  "Uses a slightly lighter shade of gold and an inverse colour-scheme with gold being the predominant colour.",
+  "Merges the versatility of the Mark XLII and the endurance of the Mark VII to combine the best features.",
+  "Features multiple arc reactors and weapons specifically designed to subdue the Hulk.",
+  "First suit to use JARVIS’ replacement, FRIDAY.",
+  "The helmet can retract into the suit, making sure Tony can talk more personally while wearing the suit.",
+  "Technically the same suit as the Mark XLVI, its main improvement being that the suit was controllable by Wi-Fi.",
+  "First suit to feature nanotechnology, making its features basically endless.",
+  "Has a slightly more robust and muscular design, compared to the Mark L.",
+];
+
+var headButton = document.querySelector("article ul:nth-of-type(2) button");
+var headText = document.querySelector("article ul:nth-of-type(3) li:nth-of-type(2)")
+
+headButton.onclick = combinedNextSuit;
+
+function headTextSwap() {
+
+  headText.innerHTML = textHead[count];
+
+  if (count>= 19) {
+    window.location.reload();
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    headText.src = textHead[count]
+
+  } else {
+    // count++;
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    headText.src = textHead[count]
+  }
+}
+
+
+// text swap BODY //
+var textBody = [
+  "Only capable of very short bursts of flight.",
+  "All parts of the body are covered, but the suit is not capable of flying at high altitudes. Also has a built in arc reactor.",
+  "Fixed the freezing problem by changing the armour to be made of a gold-titanium alloy.",
+  "Added a portable potty-system so there was no need to go to the toilet.",
+  "First and only suit to be shaped into a suitcase and is able of a quick suit-up.",
+  "Changed the arc reactor hole to a triangular shape.",
+  "Suit assembles around Iron Man through a sensor bracelet.",
+  "Has a rugged helmet and sharp angles, contrary to the main Iron Man suits.",
+  "Sacrifices strength, firepower and durability in favour of mobility.",
+  "Has one hidden blade in each arm.",
+  "Has a powerful thruster in the back to stabilize it while flying and fighting.",
+  "First suit able to be remotely controlled by chips implanted in Tony Starks arms.",
+  "Features a Sentry Mode to secure an area while Tony is out of the suit.",
+  "Developed as a contingency plan in case the Hulk lost control.",
+  "Makes use of a chromed gold titanium, making the suit considerably tougher and shinier.",
+  "This suit makes use of miniature arc reactors throughout the suit for enhanced power.",
+  "This suit makes use of silver as its main colour and could be fully operated by FRIDAY.",
+  "The suit could reshape itself in a matter of seconds and create a variety of weapons.",
+  "Made the armour able to withstand more damage and harness the power of all 6 Infinity Stones.",
+];
+
+var bodyButton = document.querySelector("article ul:nth-of-type(2) button");
+var bodyText = document.querySelector("article ul:nth-of-type(5) li:nth-of-type(2)")
+
+bodyButton.onclick = combinedNextSuit;
+
+function bodyTextSwap() {
+
+  bodyText.innerHTML = textBody[count];
+
+  if (count>= 19) {
+    window.location.reload();
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    bodyText.src = textBody[count]
+
+  } else {
+    // count++;
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    bodyText.src = textBody[count]
+  }
+}
+
+
+
+// text swap FEET //
+var textFeet = [
+  "The back and the knees were left open and were vulnerable.",
+  "Improved flight capabilities. Requires special apparatus to get in and out of the armour.",
+  "First iteration to feature the iconic red and gold colour scheme.",
+  "Higher degree of mobility.",
+  "Very high degree of mobility, but very little durability.",
+  "Increased resistance to electricity.",
+  "Introduced powerful thrusters in its back, to allow flying while in combat.",
+  "Nicknamed “Sneaky”.",
+  "Nicknamed “Nightclub”.",
+  "Nicknamed “Silver Centurion”.",
+  "Nicknamed “Shotgun”.",
+  "Relies on its high-powered repulsors to fight, along with the regular weaponry.",
+  "The suit was made to fit inside the ‘Hulkbuster’.",
+  "The suit, nicknamed “Hulkbuster”, was deployed from a satellite named Veronica and assembled around Tony’s regular suit.",
+  "Increased durability so it was able to withstand a long fight, while still being able to overload a humongous thruster.",
+  "Starks watch stored a glove with a sonar- and flashbang-blast to fight enemies without the need to war the full suit.",
+  "Has powerful, detachable jets that launch from the suit.",
+  "The design mimicked the shape of Starks body and was able to fly into deep space without any drawbacks.",
+  "Solved the limitation of FRIDAY not working in deep space.",
+]
+
+var feetButton = document.querySelector("article ul button");
+var feetText = document.querySelector("article ul:nth-of-type(4) li:nth-of-type(2)")
+
+feetButton.onclick = combinedNextSuit;
+
+function feetTextSwap() {
+
+  feetText.innerHTML = textFeet[count];
+
+  if (count>= 19) {
+    window.location.reload();
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    feetText.src = textFeet[count]
+
+  } else {
+    // count++;
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    feetText.src = textFeet[count]
+  }
+}
+
+// text swap suit name //
+var suitName = [
+  "Mark I",
+  "Mark II",
+  "Mark III",
+  "Mark IV",
+  "Mark V",
+  "Mark VI",
+  "Mark VII",
+  "Mark XV",
+  "Mark XVI",
+  "Mark XXXIII",
+  "Mark XL",
+  "Mark XLII",
+  "Mark XLIII",
+  "Mark XLIV",
+  "Mark XLV",
+  "Mark XLVI",
+  "Mark XLVII",
+  "Mark L",
+  "Mark LXXXV",
+]
+
+var suitNameButton = document.querySelector("article ul button");
+var suitNameText = document.querySelector("article ul:nth-of-type(6) li:nth-of-type(2)")
+
+suitNameButton.onclick = combinedNextSuit;
+
+function suitNameSwap() {
+
+  suitNameText.innerHTML = suitName[count];
+
+  if (count>= 19) {
+    window.location.reload();
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    suitNameText.src = suitName[count]
+
+  } else {
+    // count++;
+    // console.log(count);
+    // console.log(ironManSuitCycle[count]);
+    suitNameText.src = suitName[count]
+  }
+}
+
+
+
 function combinedNextSuit() {
+  mechanicalSounds(); 
   imageNaarVolgendSuit();
   suitFlyAnimation();
   imageNaarVolgendLogo();
-  mechanicalSounds();
+  headTextSwap();
+  bodyTextSwap();
+  feetTextSwap();
+  suitNameSwap();
 }
 
